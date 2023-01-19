c096b226463fce0992c49cc1a6dd23b0d135dafe
KBRYD151294\Kevin <newerakb@gmail.com>
2023-01-19 16:25:30 -0500

    5

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { accounts, users, writeJSON } = require('./data.js');
+const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = ('./routes/accounts.js');
+const servicesRoutes = ('./routes/services.js');
 
 const app = express();
 
@@ -15,42 +17,9 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
-app.get('/savings', (req, res) => {
-  res.render('account', { account: accounts.savings });
-});
-
-app.get('/checking', (req, res) => {
-  res.render('account', { account: accounts.checking });
-});
-
-app.get('/credit', (req, res) => {
-  res.render('account', { account: accounts.credit });
-});
-
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
-app.get('/transfer', (req, res) => res.render('transfer'));
+//app.use('/account', accountRoutes);
+//app.use('/services', servicesRoutes);
 
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
-
-app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-
-  writeJSON();
-
-  res.render('transfer', { message: "Transfer Completed" });
-});
-
-app.post('/payment', (req, res) => {
-  accounts.credit.balance = accounts.credit.balance - req.body.amount;
-  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount, 10);
-
-  writeJSON();
-
-  res.render('payment', { message: "Payment Successful", account: accounts.credit });
-});
-
-app.listen(3000, () => {
-  console.log('PS Project Running on port 3000!');
-});
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'ut
 const users = JSON.parse(userData);
 
 const writeJSON = () => {
-  const accountsJSON = JSON.stringify(accounts, null, 4);
+  let accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
 }
 
