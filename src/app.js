0e98c44c170b77af8ed3e7efb50ac6aaa25bcfad
KBRYD151294\Kevin <newerakb@gmail.com>
2023-01-19 15:45:28 -0500

    4

--- src/app.js
+++ src/app.js
@@ -36,7 +36,27 @@ app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
 app.get('/transfer', (req, res) => res.render('transfer'));
 
-app.post('/transfer', (req, res) => { });
+app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+  res.render('transfer', { message: "Transfer Completed" });
+});
+
+app.post('/payment', (req, res) => {
+  accounts.credit.balance = accounts.credit.balance - req.body.amount;
+  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount, 10);
+
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+  res.render('payment', { message: "Payment Successful", account: accounts.credit });
+});
 
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
