cc7279564deb55e10dd8df442debe35853cb30c1
KBRYD151294\Kevin <newerakb@gmail.com>
2023-01-19 15:34:05 -0500

    3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
@@ -32,6 +33,10 @@ app.get('/credit', (req, res) => {
 });
 
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+
+app.post('/transfer', (req, res) => { });
 
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
